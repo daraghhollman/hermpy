e37839a2d0a8ca7362fd76825dc3e3515ecdc4ca
Daragh Hollman <hollmandaragh@gmail.com>
2024-09-06 10:07:31 +0100

    Formatted ephemeris ticks and updated example

--- examples/loading_boundary_crossings.py
+++ examples/loading_boundary_crossings.py
@@ -32,7 +32,10 @@ ax.plot(data["date"], data["mag_total"], color="black")
 ### ---------------------------------------
 
 ax.set_ylabel("|B| [nT]")
-#ax.set_yscale("log")
+ax.set_yscale("log")
+ax.set_xmargin(0)
+ax.tick_params(which="major", direction="out", length=16, width=1.5)
+ax.tick_params(which="minor", direction="out", length=8, width=1)
 
 # Plotting crossing intervals as axvlines
 boundary_crossings.Plot_Crossing_Intervals(ax, start, end, philpott_crossings)
@@ -41,7 +44,7 @@ boundary_crossings.Plot_Crossing_Intervals(ax, start, end, philpott_crossings)
 # We need a metakernel to retrieve ephemeris information
 metakernel = "/home/daraghhollman/Main/SPICE/messenger/metakernel_messenger.txt"
 plotting_tools.Add_Tick_Ephemeris(ax, metakernel, include={
-    "date", "hours", "minutes", "range", "latitude", "longitude", "local time" 
+    "date", "hours", "minutes", "range", "latitude", "local time" 
 })
 
 plt.show()
