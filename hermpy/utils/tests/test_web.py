47df3f707880971eff143294c658394aa0996e41
Daragh Hollman <hollmandaragh@gmail.com>
2024-12-10 11:26:07 +0000

    Added test for PDS url correct

---
+++ hermpy/utils/tests/test_web.py
@@ -0,0 +1,16 @@
+import unittest
+
+import requests
+
+from hermpy.utils import Urls
+
+
+class Test_Web_Access(unittest.TestCase):
+
+    def test_PDS_BASE(self):
+        r = requests.head(Urls.PDS_BASE)
+        self.assertEqual(r.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
