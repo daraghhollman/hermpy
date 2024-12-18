3cdaffa100b263ed20a7832f5ca84d3e66c290ab
Daragh Hollman <hollmandaragh@gmail.com>
2024-12-18 14:24:12 +0000

    Updated typing checks for Get_Position()

--- hermpy/trajectory/trajectory.py
+++ hermpy/trajectory/trajectory.py
@@ -105,10 +105,10 @@ def Get_Position(
 
     with spice.KernelPool(User.METAKERNEL):
 
-        if type(date) == dt.datetime:
+        if isinstance(date, dt.datetime):
             et = spice.str2et(date.strftime("%Y-%m-%d %H:%M:%S"))
 
-        elif type(date) == list:
+        elif isinstance(date, list):
             et = spice.datetime2et(date)
 
         else:
