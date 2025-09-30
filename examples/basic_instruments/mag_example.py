13d7ec72e16611ff75abaeb4c496a9d600e2eea2
Daragh Hollman <hollmandaragh@gmail.com>
2025-09-30 14:50:10 +0100

    Simplified mag example

--- examples/basic_instruments/mag_example.py
+++ examples/basic_instruments/mag_example.py
@@ -2,28 +2,26 @@ import datetime as dt
 
 import matplotlib.pyplot as plt
 
-from hermpy import mag, plotting
+from hermpy import mag
+from hermpy.utils import User
 
-root_dir = "/home/daraghhollman/Main/data/mercury/messenger/mag/avg_1_second/"
-
-start = dt.datetime(year=2011, month=10, day=15, hour=14, minute=15, second=0)
-end = dt.datetime(year=2011, month=10, day=15, hour=15, minute=0, second=0)
+start = dt.datetime(year=2011, month=3, day=24)
+end = dt.datetime(year=2011, month=3, day=25)
 
 # Isolating only a particular portion of the files
-data = mag.Load_Between_Dates(
-    "/home/daraghhollman/Main/data/mercury/messenger/mag/avg_1_second/", start, end, strip=True, aberrate=True
-)
+data = mag.Load_Between_Dates(User.DATA_DIRECTORIES["MAG_FULL"], start, end, average=None)
 
-# This data can then be plotted using external libraries
+# This data can then be plotted using external libraries such as matplotlib
 fig, ax = plt.subplots()
 
-ax.plot(data["date"], data["|B|"])
+ax.plot(data["date"], data["|B|"], color="black", label="|B|")
+ax.plot(data["date"], data["Bx"], color="#DC267F", label="Bx")
+ax.plot(data["date"], data["By"], color="#FFB000", label="By")
+ax.plot(data["date"], data["Bz"], color="#648FFF", label="Bz")
 
-ax.set_yscale("log")
-ax.set_ylabel("|B|")
+ax.set_xlabel("Time")
+ax.set_ylabel("Magnetic Field Strength [nT]")
 
-# Plotting ephemeris information
-# We need a metakernel to retrieve ephemeris information
-plotting.Add_Tick_Ephemeris(ax)
+ax.legend()
 
 plt.show()
